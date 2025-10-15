29d810743c34f7ba46a07cefc8f60eb6ffaf4840
spiderasasen <spiderasasen@gmail.com>
2025-10-15 12:07:53 -0400

    Making a navigation made the navagation

--- JS_Files/navigation.js
+++ JS_Files/navigation.js
@@ -9,19 +9,25 @@ const add_on = document.getElementById('Add_ons');
 // Navigation for all the items
 vinyl.addEventListener('click', () => {
     window.location.href = "Other_screens/Vinyls.html";
+    console.log('Vinyls clicked');
 })
 dac.addEventListener('click', () => {
     window.location.href = "Other_screens/DAC.html";
+    console.log('dac clicked');
 })
 headphones.addEventListener('click', () => {
     window.location.href = "Other_screens/Headphones.html";
+    console.log('Headphones clicked');
 })
 vinyl_players.addEventListener('click', () => {
     window.location.href = "Other_screens/Vinyl_players.html";
+    console.log('Vinyl players clicked');
 })
 other_players.addEventListener('click', () => {
     window.location.href = "Other_screens/Other_Music_Players.html";
+    console.log('Other players clicked')
 })
 add_on.addEventListener('click', () => {
     window.location.href = "Other_screens/Add_ons_for_anything.html";
+    console.log('add ons clicked');
 })
