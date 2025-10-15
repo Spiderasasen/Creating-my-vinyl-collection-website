97ba142a54e4114807a288428303401ead855e3b
spiderasasen <spiderasasen@gmail.com>
2025-10-15 11:27:45 -0400

    Making a navigation made a navigation from one section to the other

---
+++ JS_Files/navigation.js
@@ -0,0 +1,7 @@
+// getting the ids
+const vinyl = document.getElementById('Vinyls');
+
+// for vinyl section
+vinyl.addEventListener('click', () => {
+    window.location.href = "Other_screens/Vinyls.html";
+})
